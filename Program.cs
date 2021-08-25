375dbd04283528adb8c1bc645e616e881546b080
Rajesh <undrallarajesh53@gmail.com>
2021-08-25 07:39:20 +0530

    Branch1 updated code

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace GitDemoSample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Branch1 code updated. ");
         }
     }
 }
